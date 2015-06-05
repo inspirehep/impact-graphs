fbd26138c3666a53de64aad173cc7a996240a24a
Eamonn Maguire <eamonnmag@gmail.com>
2015-06-05 16:24:59 +0100

    Styling up the swatchboard a little.

--- impact-graph.js
+++ impact-graph.js
@@ -64,23 +64,19 @@ ImpactGraph.load = function (publication_id, url, placement, width, height, opti
 
     d3.json(url, function (data) {
         var publications = ImpactGraph.functions.process_data(publication_id, data);
-
+        d3.select(placement + " a").attr('href', 'http://www.inspirehep.net/record/' + publication_id).text(publication_id);
         d3.select("#publication_info").html(data[publication_id].title);
         d3.select("#publication_id").html(publication_id)
         var svg = d3.select(placement).append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + ImpactGraph.margins.left + "," + ImpactGraph.margins.top + ")");
 
         var x_scale = d3.scale.linear()
-            .domain(d3.extent(publications, function (d) {
-                return d.year;
-            })).range([10, width - ImpactGraph.margins.left - ImpactGraph.margins.right]);
+            .domain([1965, 2015]).range([10, width - ImpactGraph.margins.left - ImpactGraph.margins.right]);
 
-        var y_scale = d3.scale.linear()
-            .domain(d3.extent(publications, function (d) {
-                return d.citation_count == 0 ? 1 : d.citation_count;
-            })).nice().range([height - ImpactGraph.margins.top - ImpactGraph.margins.bottom, 10]);
+        var y_scale = d3.scale.log()
+            .domain([1, 10000]).range([height - ImpactGraph.margins.top - ImpactGraph.margins.bottom, 10]);
 
-        svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y_scale.range()[0] + ")");
-        svg.append("g").attr("class", "y axis");
+        //svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y_scale.range()[0] + ")");
+        //svg.append("g").attr("class", "y axis");
 
         var xAxis = d3.svg.axis().scale(x_scale).orient("bottom").tickPadding(2).tickFormat(d3.format("d"));
         var yAxis = d3.svg.axis().scale(y_scale).orient("left").tickPadding(2);
@@ -105,15 +101,19 @@ ImpactGraph.load = function (publication_id, url, placement, width, height, opti
                 var focus_node_citations = data[publication_id].citations.length;
                 return focus_node_citations == 0 ? y_scale(1) : y_scale(focus_node_citations);
             })
-            .style("opacity",1);
+            .style("opacity",.6);
 
-        svg.selectAll("g.square").data(publications).enter().append("circle").attr("r", 1).attr("cx", function (d) {
+        svg.selectAll("g.square").data(publications).enter().append("circle").attr("r", function(d) {
+            return d.type == "focus" ? 2 : 1;
+        }).attr("cx", function (d) {
             return x_scale(d.year);
         }).attr("cy", function (d) {
             return d.citation_count == 0 ? y_scale(1) : y_scale(+d.citation_count);
         }).style("fill", function (d) {
             return ImpactGraph.colors(d.type);
-        }).style("opacity", 1);
+        }).style("opacity", function() {
+            return d.type == "focus" ? 1 : .5;
+        });
 
     })
 }
